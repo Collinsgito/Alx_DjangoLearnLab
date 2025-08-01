10336108dbcc41953e37e124f4a958f3319fcd2f
Collins_Gitonga <kingkayole1@gmail.com>
2025-08-01 12:39:58 +0300

    first index file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Library
+from django.views.generic import DetailView
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+    def get_queryset(self):
+        return Library.objects.prefetch_related('books__author')
