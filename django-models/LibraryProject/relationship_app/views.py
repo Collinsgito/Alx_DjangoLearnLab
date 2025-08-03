dc496e623ed45d17748a30b36cc6554c821e26da
Collins_Gitonga <kingkayole1@gmail.com>
2025-08-03 13:24:18 +0300

    Fix: updated CBV, FBV, URLs, and templates to match checker requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,13 @@
 
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from .models import Book, Library
-from django.views.generic import DetailView
 
 def list_books(request):
-    books = Book.objects.all()  # <- REQUIRED exactly like this
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # <- Match path exactly
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
-    def get_queryset(self):
-        return Library.objects.prefetch_related('books__author')
