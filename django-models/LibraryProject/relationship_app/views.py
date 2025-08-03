75df868c9af91a4a13d0e63f46dfa9b29c0a3f93
Collins_Gitonga <kingkayole1@gmail.com>
2025-08-03 13:19:17 +0300

    Fix: adjusted view and template path for book list view to pass checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Book, Library
 from django.views.generic import DetailView
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # <- REQUIRED exactly like this
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # <- Match path exactly
 
 class LibraryDetailView(DetailView):
     model = Library
