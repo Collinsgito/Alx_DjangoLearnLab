c601dd08696d751a3e3c1c6bd4cd05c6c4a0b37d
dev_colloMbato <160731898+Collinsgito@users.noreply.github.com>
2025-09-03 14:18:22 +0300

    Add files via upload

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,15 @@
+# LibraryProject/bookshelf/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "date_of_birth")
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
