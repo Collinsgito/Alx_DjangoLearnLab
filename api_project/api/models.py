b898792a712b0f857e8d6878765256f036a2e230
dev_colloMbato <160731898+Collinsgito@users.noreply.github.com>
2025-09-03 16:07:24 +0300

    Add files via upload

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return self.title
