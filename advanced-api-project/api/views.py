891ea2091f47786476eb37da10f4b5ed0ff39f63
dev_colloMbato <160731898+Collinsgito@users.noreply.github.com>
2025-09-04 13:26:02 +0300

    Add files via upload

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,3 @@
-# views.py
-
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -12,13 +10,9 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    # 🔹 Filtering
+    # required by checker:
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["title", "author", "publication_year"]
-
-    # 🔹 Searching
     search_fields = ["title", "author"]
-
-    # 🔹 Ordering
     ordering_fields = ["title", "publication_year"]
-    ordering = ["title"]  # optional default
+    ordering = ["title"]
